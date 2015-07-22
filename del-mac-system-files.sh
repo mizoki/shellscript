0dea43bfdb187ab01386315a1e983ce11c225121
mizoki <h.mizoki@gmail.com>
2015-07-22 14:49:57 +0900

    Fix a bug that the file can not be deleted

--- del-mac-system-files.sh
+++ del-mac-system-files.sh
@@ -3,5 +3,7 @@
 #
 # Ver.1.00 (2012/09/18) : 作成
 # Ver.2.00 (2015/05/08) : リソースフォークも削除するように変更
+# Ver.2.01 (2015/07/22) : ファイルが削除できない問題を修正
 
-find . -name ".DS_Store" -or -name "._*" -exec echo delete {} ";" -exec rm -rf {} ";"
+find . -name ".DS_Store" -print0 | xargs -0 rm -v
+find . -name "._*" -print0 | xargs -0 rm -v
